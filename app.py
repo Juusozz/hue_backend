4a98eb450c2334e3728bb8bf8221844d6d7fa557
Juuso <juusosavolainen1@kamk.fi>
2024-12-14 12:28:15 +0200

    backend done for now

--- app.py
+++ app.py
@@ -6,6 +6,25 @@ load_dotenv()
 BRIDGE_IP = os.getenv('BRIDGE_IP')
 USERNAME = os.getenv('API_USERNAME')
 
+app = Flask(__name__)
+
+@app.route('/trigger/api', methods = ['POST'])
+def light_switch():
+    url = f"http://{BRIDGE_IP}/api/{USERNAME}/lights/1/state"
+    light_state = get_light_info().get('state', {}).get('on', False)
+    print(light_state)
+    if light_state == True:
+        body = {"on": False}
+    else:
+        body = {"on": True}
+    response = requests.put(url, json=body)
+    if response.status_code == 200:
+        data = response.status_code
+        print(f"Success with code {data}")
+    else:
+        print(f"Failed: {response.status_code}")
+
+
 
 def get_light_info():
     url = f"http://{BRIDGE_IP}/api/{USERNAME}/lights/1"
@@ -13,9 +32,10 @@ def get_light_info():
 
     if response.status_code == 200:
         data = response.json()
-        print(data)
+        return data
     else:
         print(f"Failed: {response.status_code}")
 
 
-get_light_info()
+if __name__ == '__main__':
+    app.run(debug  = True)
