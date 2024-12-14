eff1b4481f3ebd941e8af99dbe72308d324d0983
Juuso <juusosavolainen1@kamk.fi>
2024-12-14 15:07:44 +0200

    What next?

--- app.py
+++ app.py
@@ -3,29 +3,32 @@ from dotenv import load_dotenv
 import requests
 from flask import Flask, request, jsonify
 load_dotenv()
+from flask_cors import CORS
+
 BRIDGE_IP = os.getenv('BRIDGE_IP')
 USERNAME = os.getenv('API_USERNAME')
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route('/trigger/api', methods = ['POST'])
+@app.route('/lightswitch/trigger/api', methods = ['PUT'])
 def light_switch():
     url = f"http://{BRIDGE_IP}/api/{USERNAME}/lights/1/state"
     light_state = get_light_info().get('state', {}).get('on', False)
-    print(light_state)
     if light_state == True:
         body = {"on": False}
     else:
         body = {"on": True}
     response = requests.put(url, json=body)
     if response.status_code == 200:
-        data = response.status_code
-        print(f"Success with code {data}")
+        print(f"Light toggled successfully: {body}")
+        return jsonify({"success": True, "new_state": body}), 200
     else:
-        print(f"Failed: {response.status_code}")
+        print(f"Failed to toggle light: {response.status_code}")
+        return jsonify({"success": False, "error": response.status_code}), 500
 
 
-
+@app.route('/getlightinfo/trigger/api', methods = ['GET'])
 def get_light_info():
     url = f"http://{BRIDGE_IP}/api/{USERNAME}/lights/1"
     response = requests.get(url)
@@ -35,6 +38,20 @@ def get_light_info():
         return data
     else:
         print(f"Failed: {response.status_code}")
+
+@app.route('/colorswitch/trigger/api', methods = ['PUT'])
+def color_switch():
+    url = f"http://{BRIDGE_IP}/api/{USERNAME}/lights/1/state"
+    body = request.json
+
+    response = requests.put(url, json=body)
+
+    if response.status_code == 200:
+        print(f"Light toggled successfully: {body}")
+        return jsonify({"success": True, "new_state": body}), 200
+    else:
+        print(f"Failed to toggle light: {response.status_code}")
+        return jsonify({"success": False, "error": response.status_code}), 500
 
 
 if __name__ == '__main__':
